d15e2c3e907a3215098a87c2dc1d4e358ac24694
John Gambrell <john.gambrell@gmail.com>
2017-05-30 15:02:27 -0500

    Initial commit

---
+++ BeeHive/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  BeeHive
+//
+//  Created by John Gambrell on 5/30/17.
+//  Copyright © 2017 JPG. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
