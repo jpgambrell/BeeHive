cacf8727cf071bca2ccb62a5b4dc0e37bd6e4045
John Gambrell <john.gambrell@gmail.com>
2017-06-21 16:23:36 -0500

    added %complete label to center of circle and other cleanup

--- BeeHive/BeeHive/UIControls/ProgressCircleView.swift
+++ BeeHive/BeeHive/UIControls/ProgressCircleView.swift
@@ -13,19 +13,23 @@ class ProgressCircleView: UIView {
     
     @IBInspectable var circleStrokeColor: UIColor = UIColor.orange
     @IBInspectable var backgroundCircleStrokeColor: UIColor = UIColor.lightGray
-    @IBInspectable var fillColor: UIColor = UIColor.clear
+    @IBInspectable var backgroundCircleTransparancy: Double = 1
     @IBInspectable var lineWidth: CGFloat = 10.0
-    @IBInspectable var percentageComplete: CGFloat = 40
+    @IBInspectable var percentageComplete: Int = 40
+    @IBInspectable var labelColor: UIColor = UIColor.black
+    @IBInspectable var labelFontSize: CGFloat = 40
     
-    let startAngle =  CGFloat(270).toRadians() //CGFloat(3 * Double.pi / 2.0)
+    let fillColor: UIColor = UIColor.clear
+    let startAngle =  CGFloat(270).toRadians()
     let endAngle = CGFloat(269.9).toRadians()
     var circleLayer : CAShapeLayer!
     var backgroundCircleLayer : CAShapeLayer!
+    var renderedFromIB : Bool = false
     
-    func initCircles() {
+    
+    func setupCircles() {
         backgroundCircleLayer = CAShapeLayer()
         layer.addSublayer(backgroundCircleLayer)
-        
         circleLayer = CAShapeLayer()
         layer.addSublayer(circleLayer)
     }
@@ -33,18 +37,22 @@ class ProgressCircleView: UIView {
     
     override init(frame: CGRect){
         super.init(frame: frame)
-      initCircles()
+        setupCircles()
     }
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        initCircles()
+        setupCircles()
     }
     
     override func draw(_ rect: CGRect) {
         self.backgroundColor = UIColor.clear
+        let alpha = (backgroundCircleTransparancy >= 0 && backgroundCircleTransparancy <= 1) ? backgroundCircleTransparancy : 1
+        let strokeColor = UIColor(red: backgroundCircleStrokeColor.components.red, green: backgroundCircleStrokeColor.components.green, blue: backgroundCircleStrokeColor.components.blue, alpha: CGFloat(alpha))
+        
         backgroundCircleLayer.fillColor = fillColor.cgColor
-        backgroundCircleLayer.strokeColor = backgroundCircleStrokeColor.cgColor
+        backgroundCircleLayer.strokeColor = strokeColor.cgColor
         backgroundCircleLayer.lineWidth = lineWidth;
+        
         
         circleLayer.fillColor = fillColor.cgColor
         circleLayer.strokeColor = circleStrokeColor.cgColor
@@ -57,31 +65,63 @@ class ProgressCircleView: UIView {
         backgroundCircleLayer.strokeEnd = 1
         
         circleLayer.path = circlePath.cgPath
-        circleLayer.strokeEnd = percentageComplete / 100.0
+        circleLayer.strokeEnd = (renderedFromIB) ? CGFloat(percentageComplete) / 100 : 0
+        
+        let textAttributes: [NSAttributedStringKey: AnyObject] = [
+            NSAttributedStringKey(rawValue: NSAttributedStringKey.foregroundColor.rawValue) : labelColor.cgColor,
+            NSAttributedStringKey(rawValue: NSAttributedStringKey.font.rawValue) : UIFont.systemFont(ofSize: labelFontSize)
+        ]
+         guard let context = UIGraphicsGetCurrentContext() else { return }
+        // Flip the coordinate system
+        context.textMatrix = .identity
+        context.translateBy(x: 0, y: bounds.size.height)
+        context.scaleBy(x: 1.0, y: -1.0)
+        
+        _ = drawText(context: context, text: "\(percentageComplete)%" as NSString, attributes: textAttributes)
         
     }
     
+    func drawText(context: CGContext, text: NSString, attributes: [NSAttributedStringKey: AnyObject]) -> CGSize {
+        let attributedString = NSAttributedString(string: text as String, attributes: attributes)
+        
+        let textSize = text.size(withAttributes: attributes)
+        
+        // y: Add font.descender (its a negative value) to align the text at the baseline
+        let yPos = (bounds.height/2) - (textSize.height/2)
+        let xPos = (bounds.width/2) - (textSize.width/2)
+        
+        let textPath    = CGPath(rect: CGRect(x: xPos, y: yPos, width: ceil(textSize.width), height: ceil(textSize.height)).integral, transform: nil)
+        let frameSetter = CTFramesetterCreateWithAttributedString(attributedString)
+        let frame       = CTFramesetterCreateFrame(frameSetter, CFRange(location: 0, length: attributedString.length), textPath, nil)
+        
+        CTFrameDraw(frame, context)
+        
+        return textSize
+    }
     
+    override func prepareForInterfaceBuilder() {
+        renderedFromIB = true
+    }
     
     func animateCircle(duration: TimeInterval) {
         // We want to animate the strokeEnd property of the circleLayer
-        let animation = CABasicAnimation(keyPath: "strokeEnd")
+        let animation = CABasicAnimation(keyPath: "strokePath")
         
         // Set the animation duration appropriately
         animation.duration = duration
         
         // Animate from 0 (no circle) to 1 (full circle)
         animation.fromValue = 0
-        animation.toValue = percentageComplete / 100.0
+        animation.toValue = CGFloat(percentageComplete) / 100
         
         // Do a linear animation (i.e. the speed of the animation stays the same)
-        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
         
         // Set the circleLayer's strokeEnd property to 1.0 now so that it's the
         // right value when the animation ends.
        // circleLayer.strokeColor = circleStrokeColor.cgColor
         
-        circleLayer.strokeEnd = percentageComplete / 100.0
+        circleLayer.strokeEnd = CGFloat(percentageComplete) / 100
         
         circleLayer.add(animation, forKey: "animateCircle")
         
@@ -96,4 +136,10 @@ extension CGFloat {
     }
 }
 
+extension UIColor {
+    var components: (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) {
+        let coreImageColor = CIColor(color: self)
+        return (coreImageColor.red, coreImageColor.green, coreImageColor.blue, coreImageColor.alpha)
+    }
+}
 
--- BeeHive/BeeHive/ViewController.swift
+++ BeeHive/BeeHive/ViewController.swift
@@ -13,17 +13,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var tasksProgressCircleView: ProgressCircleView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        tasksProgressCircleView.percentageComplete = 60
+        tasksProgressCircleView.circleLayer.strokeEnd = 0
         
         
     }
     override func viewDidAppear(_ animated: Bool) {
-        tasksProgressCircleView.animateCircle(duration: 1.0)
+        
+        delay(1.0) {
+            self.tasksProgressCircleView.animateCircle(duration: 0.3)
+        }
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func delay(_ seconds: Double, completion: @escaping ()->Void) {
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + .milliseconds(Int(seconds * 1000.0))) {
+            completion()
+        }
+    }
 
 }
 
